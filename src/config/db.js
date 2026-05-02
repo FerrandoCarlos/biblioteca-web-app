f79198ac5a9c286cfa16077b7ed9be0b233d814c
Ferrando Carlos <cferrando2015@gmail.com>
2026-05-02 00:54:50 -0300

    core: confu¿iguración de base de datos con patron singleton

---
+++ src/config/db.js
@@ -0,0 +1,40 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { pool } = pg;
+
+class Database {
+    static #instance = null;
+
+    constructor() {
+        this.pool = new Pool({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+        })
+    }
+
+    static getInstance() {
+        if (!Database.#instance) {
+            Database.#instance = new Database();
+        }
+        return Database.#instance;
+    }
+
+    async query(text, params = []) {
+        const client = await this.pool.connect();
+        try {
+            const result = await client.query(text, params);
+            return result;
+
+        } finally {
+            client.release()
+        }
+    }
+}
+
+export default Database.getInstance();
