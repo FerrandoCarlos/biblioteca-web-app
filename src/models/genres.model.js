d5c2f9cf85bef5e0e1cd6f7482a9d8e76a2ea17a
Ferrando Carlos <cferrando2015@gmail.com>
2026-05-02 20:29:41 -0300

    feat: se agrega el modelo de generos

---
+++ src/models/genres.model.js
@@ -0,0 +1,56 @@
+import db from '../config/db.js';
+
+class GenresModel {
+
+    async getAll() {
+        try {
+            const { rows } = await db.query('SELECT * FROM get_all_genres()');
+            return rows;
+        } catch (error) {
+            throw new Error(`Error al obtener géneros: ${error.message}`);
+        }
+    }
+
+    async getById(id) {
+        try {
+            const { rows } = await db.query(
+                'SELECT * FROM get_genre_by_id($1)', [id]
+            );
+            return rows[0] ?? null;
+        } catch (error) {
+            throw new Error(`Error al obtener géneros: ${error.message}`);
+        }
+    }
+
+    async insert(description) {
+        try {
+            await db.query(
+                'CALL insert_genre($1)', [description]
+            );
+        } catch (error) {
+            throw new Error(`Error al insertar géneros: ${error.message}`);
+        }
+    }
+
+    async update(id, description) {
+        try {
+            await db.query(
+                'CALL update_genre($1,$2)', [id, description]
+            )
+        } catch (error) {
+            throw new Error(`Error al actualizar géneros: ${error.message}`);
+        }
+    }
+
+    async delete(id) {
+        try {
+            await db.query(
+                'CALL delete_genre($1)', [id]
+            )
+        } catch (error) {
+            throw new Error(`Error al eliminar géneros: ${error.message}`);
+        }
+    }
+}
+
+export default new GenresModel();
